60f27b1513262a7c6a57b5d5f7fb7d19c516dc3c
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-24 13:57:42 +0530

    Elements adding by using Push method on Linkedlist

---
+++ MyStack.java
@@ -0,0 +1,16 @@
+package com.StackANDQueues;
+
+import java.util.LinkedList;
+
+   public class MyStack {	
+    LinkedList<Integer>newlist =new LinkedList<Integer>();
+   
+   public void Push(Integer Data) {
+	newlist.add(Data);
+   }
+  
+   public void PrintStack() {
+	System.out.println(newlist);
+	
+   } 
+}
---
+++ MyStackMain.java
@@ -0,0 +1,15 @@
+package com.StackANDQueues;
+
+public class MyStackMain {
+
+	public static void main(String[] args) {
+		
+		MyStack mystack = new MyStack();
+		mystack.Push(70);               
+		mystack.Push(30);				  
+		mystack.Push(56);				  
+		
+		mystack.PrintStack();
+
+	}
+}
